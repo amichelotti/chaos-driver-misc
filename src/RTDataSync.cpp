94e9d1aed2c638411ff0ae65dcb94dbe955defde
Andrea Michelotti <andrea.michelotti@lnf.infn.it>
2016-01-27 12:56:44 +0100

    add Data Synchronization RT

---
+++ src/RTDataSync.cpp
@@ -0,0 +1,83 @@
+/*
+ *	RTDataSync.cpp
+ *	!CHAOS
+ *	Created automatically
+ *
+ *    	Copyright 2012 INFN, National Institute of Nuclear Physics
+ *
+ *    	Licensed under the Apache License, Version 2.0 (the "License");
+ *    	you may not use this file except in compliance with the License.
+ *    	You may obtain a copy of the License at
+ *
+ *    	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    	Unless required by applicable law or agreed to in writing, software
+ *    	distributed under the License is distributed on an "AS IS" BASIS,
+ *    	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    	See the License for the specific language governing permissions and
+ *    	limitations under the License.
+ */
+
+#include "RTDataSync.h"
+//#include "RTDataSyncDriver.h"
+#include <boost/algorithm/string.hpp>
+#include <chaos/ui_toolkit/LowLevelApi/LLRpcApi.h>
+#include <chaos/cu_toolkit/command_manager/CommandManager.h>
+#include <common/debug/core/debug.h>
+using namespace chaos;
+using namespace chaos::common::data::cache;
+using namespace chaos::cu::driver_manager::driver;
+using namespace ::driver::daq::libera;
+using namespace ::driver::misc;
+PUBLISHABLE_CONTROL_UNIT_IMPLEMENTATION(RTDataSync)
+
+#define RTDataSyncLAPP_		LAPP_ << "[RTDataSync] "
+#define RTDataSyncLDBG_		LDBG_ << "[RTDataSync] " << __PRETTY_FUNCTION__ << " "
+#define RTDataSyncLERR_		LERR_ << "[RTDataSync] " << __PRETTY_FUNCTION__ << "("<<__LINE__<<") "
+
+
+
+/*
+ Construct
+ */
+RTDataSync::RTDataSync(const string& _control_unit_id, const string& _control_unit_param, const ControlUnitDriverList& _control_unit_drivers):
+RTAbstractControlUnit(_control_unit_id, _control_unit_param, _control_unit_drivers) {
+    int cnt=0;
+    std::vector<std::string>::iterator i;
+   rem_variables=0;
+     boost::split(cu_names,_control_unit_param,boost::is_any_of(" \n"));
+     rem_variables=cu_names.size();
+     DPRINT("Variables :%d",rem_variables);
+     if(rem_variables>0){
+         rem_data = new driver::misc::ChaosDatasetAttribute*[rem_variables];
+     } else {
+         return;
+     }
+
+     data_group=new ChaosDatasetAttributeSinchronizer();
+    if(data_group==NULL){
+        throw chaos::CException(-100,"## cannot create group access",__PRETTY_FUNCTION__);
+
+    }
+     i=cu_names.begin();
+
+     for(cnt=0;cnt<rem_variables;cnt++){
+         DPRINT("[%d] Adding Remote Variable \"%s\"",cnt*i);
+         rem_data[cnt] = new ChaosDatasetAttribute(*i);
+         data_group->add(rem_data[cnt] );
+         i++;
+     }
+     
+   
+    data_group->setInterval(3000000);
+    data_group->setTimeout (6000000);
+  
+    
+}
+
+/*
+ Destructor
+ */
+RTDataSync::~RTDataSync() {
+
+}
---
+++ src/RTDataSync.h
@@ -0,0 +1,55 @@
+/*
+ *	RTDataSync.h
+ *	!CHAOS
+ *	Created by Andrea Michelotti
+ *      Collects and align a given series of Libera BPMs
+ *    	Copyright 2015 INFN, National Institute of Nuclear Physics
+ *
+ *    	Licensed under the Apache License, Version 2.0 (the "License");
+ *    	you may not use this file except in compliance with the License.
+ *    	You may obtain a copy of the License at
+ *
+ *    	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    	Unless required by applicable law or agreed to in writing, software
+ *    	distributed under the License is distributed on an "AS IS" BASIS,
+ *    	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    	See the License for the specific language governing permissions and
+ *    	limitations under the License.
+ */
+#ifndef _RTDataSync_h
+#define _RTDataSync_h
+
+#include <chaos/cu_toolkit/control_manager/RTAbstractControlUnit.h>
+#include <driver/misc/ChaosControllerGroup.h>
+#include <driver/misc/ChaosDatasetAttribute.h>
+#include <driver/misc/ChaosDatasetAttributeSinchronizer.h>
+#include <driver/daq/models/Libera/ChaosControllerLibera.h>
+
+   
+    namespace driver {
+        
+        namespace daq {
+            namespace libera{
+class RTDataSync : public chaos::cu::control_manager::RTAbstractControlUnit {
+	PUBLISHABLE_CONTROL_UNIT_INTERFACE(RTDataSync)
+public:
+    /*!
+     Construct a new CU with full constructor
+     */
+    RTDataSync(const std::string& _control_unit_id, const std::string& _control_unit_param, const ControlUnitDriverList& _control_unit_drivers);
+    /*!
+     Destructor a new CU
+     */
+    ~RTDataSync();
+
+protected:
+        driver::misc::ChaosDatasetAttribute** rem_data;
+        int rem_variables;
+        driver::misc::ChaosDatasetAttributeSinchronizer* data_group;
+        std::vector<std::string> cu_names;
+
+
+};
+            }}}
+#endif
