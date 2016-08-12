184572ce1c92bbaee7c509221c0315406cb44b4e
Andrea Michelotti <andrea.michelotti@lnf.infn.it>
2016-08-12 16:00:14 +0200

    not last push

--- models/benchmark/CmdCalcBandwidth.cpp
+++ models/benchmark/CmdCalcBandwidth.cpp
@@ -25,31 +25,31 @@ CmdCalcBandwidth::CmdCalcBandwidth()  {
 	repeat=0;
 	counter=0;
 	start = 0;
-        samples=0;
-        repetition_stats=NULL;
+	samples=0;
+	repetition_stats=NULL;
 	setFeatures(chaos_batch::features::FeaturesFlagTypes::FF_SET_SCHEDULER_DELAY, (uint64_t)0);
-	
+
 }
 
 CmdCalcBandwidth::~CmdCalcBandwidth() {
 	/*if(buffer) free(buffer);
 	buffer = 0;*/
-    if(repetition_stats){
-        free(repetition_stats);
-        repetition_stats=NULL;
-    }
-	
+	if(repetition_stats){
+		free(repetition_stats);
+		repetition_stats=NULL;
+	}
+
 }
 
 // return the implemented handler
 uint8_t CmdCalcBandwidth::implementedHandler() {
 	return  chaos_batch::HandlerType::HT_Set | chaos_batch::HandlerType::HT_Acquisition;
-	
+
 }
 
 // Start the command execution
 void CmdCalcBandwidth::setHandler(CDataWrapper *data) {
-	
+
 	if(!data) {
 		std::cout<<"## empty data"<<std::endl;
 		return;
@@ -57,104 +57,104 @@ void CmdCalcBandwidth::setHandler(CDataWrapper *data) {
 	int64_t ts_tag;
 	hdr = getAttributeCache()->getRWPtr<MessProfilePacketInfo>(DOMAIN_OUTPUT, "profile");
 	memset((void*)hdr,0,sizeof(hdr));
-	
+
 	boost::posix_time::ptime time = boost::posix_time::microsec_clock::local_time();
 	hdr->tprof.cmd_arrival_time_us = time.time_of_day().total_microseconds();
 	ts_tag = data->getUInt64Value("ts_tag");
 	size = data->getUInt32Value(CmdCalcBandwidth_BYTES_PARAM_KEY);
 	//enalrge cache
 	getAttributeCache()->setOutputAttributeNewSize("buffer", size);
-	
+
 	hdr->buffsize = size;
 	hdr->uidx= 0;
 	hdr->ts_tag = ts_tag;
-        hdr->tprof.cycle_sigma=0;
+	hdr->tprof.cycle_sigma=0;
 	buffer = getAttributeCache()->getRWPtr<uint8_t>(DOMAIN_OUTPUT, "buffer");
-        repeat = data->getUInt32Value(CmdCalcBandwidth_REPEAT_PARAM_KEY);
-        repetition_stats=(int64_t*)realloc(repetition_stats,repeat*sizeof(int64_t));
-        assert(repetition_stats);
-        LAPP_<<"Command bytes:"<<size<<" repeat:"<<repeat<<endl;
-        getAttributeCache()->setOutputAttributeNewSize("buffer",size);
+	repeat = data->getUInt32Value(CmdCalcBandwidth_REPEAT_PARAM_KEY);
+	repetition_stats=(int64_t*)realloc(repetition_stats,repeat*sizeof(int64_t));
+	assert(repetition_stats);
+	LAPP_<<"Command bytes:"<<size<<" repeat:"<<repeat<<endl;
+	getAttributeCache()->setOutputAttributeNewSize("buffer",size);
 	assert(buffer);
 	//start =1;
 	counter = 0;
-        oldtime =0 ;
-        samples=0;
-        avg_cycle =0;
-        avg_net_cycle=0;
-        hdr->uidx=-1;
-        getAttributeCache()->setOutputDomainAsChanged();
+	oldtime =0 ;
+	samples=0;
+	avg_cycle =0;
+	avg_net_cycle=0;
+	hdr->uidx=-1;
+	getAttributeCache()->setOutputDomainAsChanged();
 	// initialize counter
 	// acquiredData->addBinaryValue("profile", (char*)&hdr, sizeof(hdr));
 	//    pushDataSet(acquiredData);
 	usleep(500000); // be sure ui is in the main loop
 
-    
-	
+
+
 }
 
 void CmdCalcBandwidth::acquireHandler() {
-     
+
 	if((counter<repeat)) {
-	  //		r_lock->lock();
+		//		r_lock->lock();
 
 		st_prep= boost::posix_time::microsec_clock::local_time();
-                st_time=st_prep.time_of_day().total_microseconds();
+		st_time=st_prep.time_of_day().total_microseconds();
 		hdr->uidx=counter;
-                if(counter==0){
-                    start_cycle=st_time;
-                }
-#ifdef CHECK_BUFFER
+		if(counter==0){
+			start_cycle=st_time;
+		}
+	#ifdef CHECK_BUFFER
 		boost::crc_32_type crc_calc;
 		boost::random::random_device r;
 		r.generate(buffer, buffer+size);
 		crc_calc.process_bytes(buffer, size);
 		hdr->crc32 =crc_calc.checksum();
-#endif
-		end_prep= boost::posix_time::microsec_clock::local_time();
-                prep_time = ( end_prep - st_prep).total_microseconds();
-		hdr->tprof.prepare_time_us = prep_time;
-                if(oldtime>0){
-                                                
+	#endif
+end_prep= boost::posix_time::microsec_clock::local_time();
+prep_time = ( end_prep - st_prep).total_microseconds();
+hdr->tprof.prepare_time_us = prep_time;
+if(oldtime>0){
 
-                    uint64_t tm=st_time - oldtime;
-                    avg_cycle += tm;
-                    repetition_stats[samples]=tm;
-                    samples++;
-                    avg_net_cycle +=tm - prep_time;
-                   
-                    LDBG_<<"counter:"<<counter<<" cycle avg:"<<hdr->tprof.cycle_us;
-                    if(counter == (repeat-1)){
 
-                        if(samples>0){
-                            int cnt;
-			    uint64_t tot_time=end_prep.time_of_day().total_microseconds() -start_cycle;                            double val=0;
+	uint64_t tm=st_time - oldtime;
+	avg_cycle += tm;
+	repetition_stats[samples]=tm;
+	samples++;
+	avg_net_cycle +=tm - prep_time;
 
-                            hdr->tprof.net_cycle_us=avg_net_cycle/samples;
-                            hdr->tprof.cycle_us=(double)avg_cycle/samples;
-                            for(cnt=0;cnt<samples;cnt++){
-                                val+=pow(((double)repetition_stats[cnt] - hdr->tprof.cycle_us),2);
-                                 LDBG_<<" val:"<<repetition_stats[cnt];                              
-                            }
+	LDBG_<<"counter:"<<counter<<" cycle avg:"<<hdr->tprof.cycle_us;
+	if(counter == (repeat-1)){
 
-			    val=val/samples;
-			    hdr->tprof.cycle_sigma=sqrt(val);
-			    hdr->tprof.cycle_sec=repeat*1000000.0/tot_time;
-			
-			    LAPP_<<" Avg:"<<hdr->tprof.cycle_us<<" Sigma:"<<hdr->tprof.cycle_sigma<<" cycle/s "<<hdr->tprof.cycle_sec;
+		if(samples>0){
+			int cnt;
+			uint64_t tot_time=end_prep.time_of_day().total_microseconds() -start_cycle;                            double val=0;
 
+			hdr->tprof.net_cycle_us=avg_net_cycle/samples;
+			hdr->tprof.cycle_us=(double)avg_cycle/samples;
+			for(cnt=0;cnt<samples;cnt++){
+				val+=pow(((double)repetition_stats[cnt] - hdr->tprof.cycle_us),2);
+				LDBG_<<" val:"<<repetition_stats[cnt];
 			}
-                    }
 
-                }
-                oldtime=st_time;
-		hdr->tprof.end_cycle_us=end_prep.time_of_day().total_microseconds();
-                
-		counter++;
-		//	r_lock->unlock();
-		//force changed domain
+			val=val/samples;
+			hdr->tprof.cycle_sigma=sqrt(val);
+			hdr->tprof.cycle_sec=repeat*1000000.0/tot_time;
+
+			LAPP_<<" Avg:"<<hdr->tprof.cycle_us<<" Sigma:"<<hdr->tprof.cycle_sigma<<" cycle/s "<<hdr->tprof.cycle_sec;
+
+		}
+	}
+
+}
+oldtime=st_time;
+hdr->tprof.end_cycle_us=end_prep.time_of_day().total_microseconds();
+getAttributeCache()->setOutputDomainAsChanged();
+
+//	r_lock->unlock();
+//force changed domain
 	} else {
-         /*   int cnt;
+		/*   int cnt;
             double val=0;
             if(samples>0){
                 for(cnt=0;cnt<samples;cnt++){
@@ -163,12 +163,12 @@ void CmdCalcBandwidth::acquireHandler() {
                 val=val/samples;
                 hdr->tprof.cycle_sigma=sqrt(val);
                // LAPP_<<" Avg:"<<hdr->tprof.cycle_us<<" Sigma:"<<hdr->tprof.cycle_sigma;
-                
+
             }*/
-            BC_END_RUNNIG_PROPERTY;
-        }
-        
-        getAttributeCache()->setOutputDomainAsChanged();
+		BC_END_RUNNIG_PROPERTY;
+
+	}
+	counter++;
 
 }
 
