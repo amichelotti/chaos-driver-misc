933d9c6c4ea187dc6e5e5dba411df8f125db5bbd
amichelo <andrea.michelotti@lnf.infn.it>
2017-06-20 17:50:24 +0200

    new files

---
+++ models/cernRoot/chaosRoot.cpp
@@ -0,0 +1,48 @@
+/* 
+ * File:   testDataSetAttribute.cpp
+ * Author: michelo
+ * test from UI
+ * Created on September 10, 2015, 11:24 AM
+ */
+
+#include <ChaosMetadataServiceClient/ChaosMetadataServiceClient.h>
+
+#include <driver/misc/core/ChaosController.h>
+using namespace std;
+#include "TROOT.h"
+#include "TRint.h"
+#include <driver/misc/models/cernRoot/rootUtil.h>
+/*
+ * 
+ */
+using namespace driver::misc;
+using namespace chaos::metadata_service_client;
+
+int main(int argc, char** argv) {
+	std::string rootopt;
+	char* root_opts[120];
+	int nroot_opts=0;
+	std::string buf;
+	ChaosMetadataServiceClient::getInstance()->getGlobalConfigurationInstance()->addOption("rootopt", po::value<string>(&rootopt), "Options to give tu CERN ROOT interpreter ");
+	ChaosMetadataServiceClient::getInstance()->init(argc,argv);
+	ChaosMetadataServiceClient::getInstance()->start();
+	 if (0) {
+		 TTree* tt= queryChaosTree("buttami","-3m","-1",0,"");
+		 printf("%x",tt);
+
+	 }
+	 root_opts[nroot_opts++]=argv[0];
+
+	 stringstream ss(rootopt);
+	 while (ss >> buf){
+		 root_opts[nroot_opts++]=strdup(buf.c_str());
+	 }
+
+
+	TRint *rootapp = new TRint("Rint", &nroot_opts, root_opts);
+	rootapp->SetPrompt("chaosRoot[%d]>");
+	  rootapp->Run();
+
+    return 0;
+}
+
