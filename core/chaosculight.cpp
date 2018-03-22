0412d75e669ebe626ccdcc81162a621a3b82c5cc
Andrea Michelotti <andrea.michelotti@lnf.infn.it>
2018-03-22 17:42:24 +0100

    pushing cu

--- core/chaosculight.cpp
+++ core/chaosculight.cpp
@@ -1,6 +1,66 @@
 #include "chaosculight.h"
 
-ChaosCULight::ChaosCULight()
-{
+#define DPD_LOG_HEAD "[ChaosCULight] - "
 
+#define DPD_LAPP LAPP_ << DPD_LOG_HEAD
+#define DPD_LDBG LDBG_ << DPD_LOG_HEAD << __PRETTY_FUNCTION__
+#define DPD_LERR LERR_ << DPD_LOG_HEAD << __PRETTY_FUNCTION__ << "(" << __LINE__ << ") "
+
+
+namespace driver{
+namespace misc{
+ChaosCULight::ChaosCULight(){
+    network_broker=NetworkBroker::getInstance();
+    mds_message_channel = network_broker->getMetadataserverMessageChannel();
+    if(!mds_message_channel)
+        throw chaos::CException(-1, "No mds channel found", __PRETTY_FUNCTION__);
+
+    //! get the direct io client
+    direct_io_client = network_broker->getSharedDirectIOClientInstance();
+    //checkif someone has passed us the device indetification
+    DPD_LAPP << "Scan the direction address";
+
+    CDataWrapper *tmp_data_handler = NULL;
+
+    if(!mds_message_channel->getDataDriverBestConfiguration(&tmp_data_handler, 5000)){
+        if(tmp_data_handler!=NULL){
+            ChaosUniquePtr<chaos::common::data::CDataWrapper> best_available_da_ptr(tmp_data_handler);
+            DPD_LDBG <<best_available_da_ptr->getJSONString();
+            ChaosUniquePtr<chaos::common::data::CMultiTypeDataArrayWrapper> liveMemAddrConfig(best_available_da_ptr->getVectorValue(DataServiceNodeDefinitionKey::DS_DIRECT_IO_FULL_ADDRESS_LIST));
+            if(liveMemAddrConfig.get()){
+                size_t numerbOfserverAddressConfigured = liveMemAddrConfig->size();
+                for ( int idx = 0; idx < numerbOfserverAddressConfigured; idx++ ){
+                    std::string serverDesc = liveMemAddrConfig->getStringElementAtIndex(idx);
+                    connection_feeder.addURL(serverDesc);
+                }
+            }
+        }
+    }
+}
+ChaosCULight::~ChaosCULight(){
+    connection_feeder.clear();
+
+    //if(direct_io_client) {
+    //CHAOS_NOT_THROW(InizializableService::deinitImplementation(direct_io_client,
+    //														   direct_io_client->getName(),
+    //														   __PRETTY_FUNCTION__);)
+    //delete(direct_io_client);
+    //}
+
+    if(mds_message_channel) network_broker->disposeMessageChannel(mds_message_channel);
+}
+int ChaosCULight::registerCU (const std::string&name,const chaos::common::data::CDataWrapper& dataset){
+    if(network_broker == NULL){
+    }
+
+
+
+}
+int ChaosCULight::pushDataset(const chaos::common::data::CDataWrapper& dataset){
+
+}
+int ChaosCULight::deinit(){
+
+}
+}
 }
--- core/chaosculight.h
+++ core/chaosculight.h
@@ -2,10 +2,36 @@
 #define CHAOSCULIGHT_H
 
 
-class ChaosCULight
-{
-public:
-    ChaosCULight();
+#include <chaos/common/data/CDataWrapper.h>
+#include <chaos/common/network/URLServiceFeeder.h>
+#include <chaos/common/direct_io/DirectIOClient.h>
+#include <chaos/common/message/MDSMessageChannel.h>
+#include <chaos/common/utility/InizializableService.h>
+#include <chaos/common/direct_io/DirectIOClientConnection.h>
+#include <chaos/common/network/NetworkBroker.h>
+namespace driver{
+namespace misc{
+
+class ChaosCULight{
+
+
+    chaos::common::network::NetworkBroker		*network_broker;
+    chaos::common::direct_io::DirectIOClient	*direct_io_client;
+    chaos::common::message::MDSMessageChannel	*mds_message_channel;
+
+    chaos::common::network::URLServiceFeeder	connection_feeder;
+
+    public:
+        ChaosCULight();
+        ~ChaosCULight();
+        /**
+
+        */
+        int registerCU (const std::string&name,const chaos::common::data::CDataWrapper& dataset);
+        int pushDataset(const chaos::common::data::CDataWrapper& dataset);
+        int deinit();
+
 };
+}}
 
 #endif // CHAOSCULIGHT_H
