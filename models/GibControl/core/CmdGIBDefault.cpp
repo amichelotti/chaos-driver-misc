2dab151525a32200ad90594f1da867e158aa3937
aduffizi <alessandro.duffizi@lnf.infn.it>
2018-08-20 14:06:12 +0200

    git merge problems fixed

--- models/GibControl/core/CmdGIBDefault.cpp
+++ models/GibControl/core/CmdGIBDefault.cpp
@@ -28,16 +28,10 @@ limitations under the License.
 namespace own = driver::gibcontrol;
 namespace c_data =  chaos::common::data;
 namespace chaos_batch = chaos::common::batch_command;
-<<<<<<< HEAD:models/GibControl/core/CmdGIBDefault.cpp
-BATCH_COMMAND_OPEN_DESCRIPTION_ALIAS(driver::gibcontrol::,CmdGIBDefault,CMD_GIB_DEFAULT_ALIAS,
-			"Default command",
-			"d3bdc407-89f1-404d-bd67-8a07459f6193")
-=======
 using namespace chaos::cu::control_manager;
 BATCH_COMMAND_OPEN_DESCRIPTION_ALIAS(driver::gibcontrol::,CmdGIBDefault,CMD_GIB_DEFAULT_ALIAS,
 			"Default command",
-			"f1fdea61-40d4-4ab7-8021-6972946b31cb")
->>>>>>> 343de0cb8b58c57dcb21771470d9b86892d59627:models/GibControl/core/CmdGIBDefault.cpp
+			"d3bdc407-89f1-404d-bd67-8a07459f6193")
 BATCH_COMMAND_CLOSE_DESCRIPTION()
 
 
@@ -48,30 +42,19 @@ uint8_t own::CmdGIBDefault::implementedHandler(){
 // empty set handler
 void own::CmdGIBDefault::setHandler(c_data::CDataWrapper *data) {
 	AbstractGibControlCommand::setHandler(data);
-<<<<<<< HEAD:models/GibControl/core/CmdGIBDefault.cpp
-=======
 	SCLAPP_ <<"GIB Set Handler Default" ;
->>>>>>> 343de0cb8b58c57dcb21771470d9b86892d59627:models/GibControl/core/CmdGIBDefault.cpp
 	BC_NORMAL_RUNNING_PROPERTY
 }
 // empty acquire handler
 void own::CmdGIBDefault::acquireHandler() {
-<<<<<<< HEAD:models/GibControl/core/CmdGIBDefault.cpp
 }
 // empty correlation handler
 void own::CmdGIBDefault::ccHandler() {
-=======
 	SCLAPP_ <<"GIB ACQUIRE Handler Default" ;
         o_status_id=getAttributeCache()->getRWPtr<int32_t>(DOMAIN_OUTPUT,"status_id");
 	*o_status_id=12;
 
 	getAttributeCache()->setOutputDomainAsChanged();
-}
-// empty correlation handler
-void own::CmdGIBDefault::ccHandler() {
-	SCLAPP_ <<"GIB CC Handler Default" ;
-	//BC_END_RUNNING_PROPERTY;
->>>>>>> 343de0cb8b58c57dcb21771470d9b86892d59627:models/GibControl/core/CmdGIBDefault.cpp
 }
 // empty timeout handler
 bool own::CmdGIBDefault::timeoutHandler() {
--- models/GibControl/core/CmdGIBsetPulse.cpp
+++ models/GibControl/core/CmdGIBsetPulse.cpp
@@ -30,11 +30,7 @@ namespace c_data =  chaos::common::data;
 namespace chaos_batch = chaos::common::batch_command;
 BATCH_COMMAND_OPEN_DESCRIPTION_ALIAS(driver::gibcontrol::,CmdGIBsetPulse,CMD_GIB_SETPULSE_ALIAS,
 			"pulse state (0:off, 1:on)",
-<<<<<<< HEAD
-			"62344adc-89a9-4506-be63-a669b3dc48fc")
-=======
 			"055a00fc-de7c-4ed4-a5bb-a095ff86161a")
->>>>>>> 343de0cb8b58c57dcb21771470d9b86892d59627
 BATCH_COMMAND_ADD_INT32_PARAM(CMD_GIB_SETPULSE_CHANNEL,"channel to pulse",chaos::common::batch_command::BatchCommandAndParameterDescriptionkey::BC_PARAMETER_FLAG_MANDATORY)
 BATCH_COMMAND_ADD_INT32_PARAM(CMD_GIB_SETPULSE_AMPLITUDE,"amplitude of pulse (V)",chaos::common::batch_command::BatchCommandAndParameterDescriptionkey::BC_PARAMETER_FLAG_MANDATORY)
 BATCH_COMMAND_ADD_INT32_PARAM(CMD_GIB_SETPULSE_WIDTH,"pulse width (ns)",chaos::common::batch_command::BatchCommandAndParameterDescriptionkey::BC_PARAMETER_FLAG_MANDATORY)
