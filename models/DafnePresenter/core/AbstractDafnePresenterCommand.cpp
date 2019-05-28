202e57a698ee2330026c8a384123f931e16d548b
aduffizi <alessandro.duffizi@lnf.infn.it>
2019-05-28 14:09:02 +0200

    added CCALT

--- models/DafnePresenter/core/AbstractDafnePresenterCommand.cpp
+++ models/DafnePresenter/core/AbstractDafnePresenterCommand.cpp
@@ -43,8 +43,10 @@ void AbstractDafnePresenterCommand::setHandler(c_data::CDataWrapper *data) {
 	//get pointer to the output dataset variable
 	//o_status_id = getAttributeCache()->getRWPtr<int32_t>(DOMAIN_OUTPUT, "status_id");
 	//o_alarms = getAttributeCache()->getRWPtr<uint64_t>(DOMAIN_OUTPUT, "alarms"); 
-	this->CCALT= new  ::driver::misc::ChaosController("CCALT/GIB/GIB1");
-	this->VUGImporter= new  ::driver::misc::ChaosController("DAFNE/IMPORT/VUG");
+	const char*vugNamePointer= getAttributeCache()->getROPtr<char>(DOMAIN_CUSTOM,"CUvugImportName");
+	const char*lumiNamePointer = getAttributeCache()->getROPtr<char>(DOMAIN_CUSTOM,"CULuminometerCCALT");
+	this->CCALT= new  ::driver::misc::ChaosController(lumiNamePointer);
+	this->VUGImporter= new  ::driver::misc::ChaosController(vugNamePointer);
 	//setting default timeout (usec) 
 	const int32_t *userTimeout=getAttributeCache()->getROPtr<int32_t>(DOMAIN_INPUT,"driver_timeout");
 	if (*userTimeout > 0)
